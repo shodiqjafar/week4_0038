c684580cdf7e77090030968e4b87721e14b1f572
shodiqjafar <shodiqj742@gmail.com>
2025-10-22 15:37:37 +0700

    add scanner and input

--- BelajarVariable.java
+++ BelajarVariable.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class BelajarVariable{
     public static void main(String[] args) {
         int nUmur;
+        Scanner ns = new Scanner(System.in);
+
+        System.out.println("Masukan umur");
+        nUmur = ns.nextInt();
+
     }
 }
