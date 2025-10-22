3c2a76979a509799a05576caadf3daaa2012f82b
shodiqjafar <shodiqj742@gmail.com>
2025-10-22 15:48:51 +0700

    add string test

--- BelajarVariable.java
+++ BelajarVariable.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class BelajarVariable{
     public static void main(String[] args) {
         int nUmur;
+        String test;
         Scanner ns = new Scanner(System.in);
 
         System.out.println("Masukan umur");
         nUmur = ns.nextInt();
+
 
     }
 }
